f0c72269a4c26bb726b46ad8dc451538cd7fee4e
balwiinder <75874905+balwiinder@users.noreply.github.com>
2021-09-01 04:23:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var  database;
+var gameState=0,playerCount;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+
+  }
+
+function draw(){
+  
+  
+    }
